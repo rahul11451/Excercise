145d0333bb50a6f6c274eabead6ca32a024546f3
Nensi Darji <74348679+Nensi02@users.noreply.github.com>
2023-06-20 18:27:30 +0530

    Update RegistrationController.php

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Personal_info;
 
 class RegistrationController extends Controller
 {
@@ -17,9 +18,34 @@ class RegistrationController extends Controller
             [
                 'name' => 'required',
                 'email' => 'required|email',
-                'msg' => 'required|alpha'
+                'msg' => 'required'
             ]
         );
-        echo '<br>Nensi<br>File: '. __FILE__.'<br>Line: '.__LINE__.'<br><pre>';print_r($postData->all());echo '</pre>'; die();
+
+        $pI = new Personal_info();
+        $pI->name = $postData['name'];
+        $pI->email = $postData['email'];
+        $pI->address = $postData['msg'];
+        $pI->save();
+        return redirect('/viewtable');
+    }
+
+    public function view()
+    {
+        $reData = Personal_info::all();
+        $data = compact('reData');
+        return view('viewtable')->with($data);
+    }
+
+    public function delete($id)
+    {
+        $deletedValue = Personal_info::find($id)->delete();
+        return redirect()->back();
+    }
+
+    public function edit($id)
+    {
+        $deletedValue = Personal_info::find($id)->delete();
+        return redirect()->back();
     }
 }
